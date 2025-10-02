73a2fa8a0ef392b920243284b76826d0381dcc33
Justin Simon <justinsimoncs@gmail.com>
2025-10-02 08:37:53 -0700

    Added dependencies for db and created dbcontext class

--- Models/Diet.cs
+++ Models/Diet.cs
@@ -1,12 +1,16 @@
-﻿namespace GetFit_Application.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GetFit_Application.Models
 {
     public class Diet
     {
         //Models to reflext the Macro info in the dashboard
+        [Required]
+
         public int Id { get; set; }
         public string Food { get; set; }
         public int Calories { get; set; }
-        public DateTime Date { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
 
         public string Carbohyrdates { get; set; }
         public string Protein { get; set; }
--- Models/Workout.cs
+++ Models/Workout.cs
@@ -2,5 +2,16 @@
 {
     public class Workout
     {
+
+        //These models reflect the exercise portion of the project, differing from the Diet dashboard.
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+
+        public string Exercise { get; set; }
+
+        public int Sets { get; set; }
+        public int Reps { get; set; }
+        public int PR { get; set; }
+
     }
 }
